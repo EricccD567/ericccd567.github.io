9d6389dce67a16e12182629394639aa961c88141
ericcd <eric.dai2194@gmail.com>
2024-08-11 22:18:03 +1000

    More boilerplate and initial asset setup

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { k } from './kaboomCtx';
+
+// vite if in public folder then direct access with ./
+k.loadSprite('spritesheet', './spritesheet.png', {
+  sliceX: 39, // 624 (img length) / 16
+});
