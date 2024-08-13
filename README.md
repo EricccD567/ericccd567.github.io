4a4987263a5d9115ba68837095567a3650acfa88
ericcd <eric.dai2194@gmail.com>
2024-08-14 02:33:37 +1000

    Created map and added more assets

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Personal Portfolio
 
+I made the map using Tiled and the asset packs listed below.
+
 ## Sources
 
+- <https://thorbjorn.itch.io/tiled>
 - <https://kenmi-art.itch.io/cute-fantasy-rpg>
+- <https://emanuelledev.itch.io/farm-rpg>
 - <https://momen-games.itch.io/happy-la-v2-ts>
 - <https://danieldiggle.itch.io/sunnyside>
 - <https://penzilla.itch.io/top-down-retro-interior>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { k } from './kaboomCtx';
 
-// vite if in public folder then direct access with ./
-k.loadSprite('spritesheet', './spritesheet.png', {
-  sliceX: 39, // 624 (img length) / 16
-  sliceY: 31, // height
-  anims: {},
+// with vite, if in public folder then have direct access with ./
+k.loadSprite('spritesheet', './spritesheet-player.png', {
+  sliceX: 4, // width 64 / 16
+  sliceY: 6, // height 96 / 16
+  anims: {
+    // play around with speed
+    'idle-down': { from: 0, to: 3, loop: true, speed: 8 },
+    'idle-up': { from: 4, to: 7, loop: true, speed: 8 },
+    'idle-side': { from: 8, to: 11, loop: true, speed: 8 },
+    'walk-down': { from: 12, to: 15, loop: true, speed: 8 },
+    'walk-up': { from: 16, to: 19, loop: true, speed: 8 },
+    'walk-side': { from: 20, to: 23, loop: true, speed: 8 },
+  },
 });
