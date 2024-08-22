980da01b882fc2777d469ced3fec3f9eff1a9aea
ericcd <eric.dai2194@gmail.com>
2024-08-22 13:19:50 +1000

    Finished all UI plus testing

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 Based on [video](https://www.youtube.com/watch?v=wy_fSStEgMs) by JSLegendDev.
 
-I made the map using Tiled and the asset packs listed below.
+I made the map using [Tiled](https://thorbjorn.itch.io/tiled) and the asset packs listed below.
+
+I made the UI using [NES.css](https://github.com/nostalgic-css/NES.css) and [system.css](https://github.com/sakofchit/system.css) components.
+
+Kirby game clone tutorial by JSLegendDev: <https://www.youtube.com/watch?v=R6WvJOiX99s>
 
 ## Sources
 
-- <https://thorbjorn.itch.io/tiled>
 - <https://kenmi-art.itch.io/cute-fantasy-rpg>
 - <https://emanuelledev.itch.io/farm-rpg>
 - <https://momen-games.itch.io/happy-la-v2-ts>
--- src/constants.js
+++ src/constants.js
@@ -33,9 +33,87 @@ export const scaleFactor = 3.5;
     <span
       class="ui-text"
       style="font-size: 2.5rem; vertical-align: top"
-      >2/5</span
     >
+      2/5
+    </span>
     <i class="nes-icon is-medium star"></i>
   </div>
+</div> */
+}
+
+{
+  /* <div
+  style="
+    margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  "
+>
+  <h2 style="font-size: clamp(1.5rem, 3vw, 2.5rem)">
+    Tuesday
+  </h2>
+  <p style="font-size: clamp(0.9rem, 1.8vw, 1.5rem)">
+    <strong>2022</strong>
+  </p>
+  <div
+    style="
+      font-size: clamp(0.5rem, 1vw, 0.8rem);
+      margin-bottom: 0.5rem;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 0.2rem 0.8rem;
+    "
+  >
+    <a class="nes-badge" style="cursor: default">
+      <span class="is-dark">Frontend</span>
+    </a>
+    <a class="nes-badge" style="cursor: default">
+      <span class="is-dark">Backend</span>
+    </a>
+    <a class="nes-badge" style="cursor: default">
+      <span class="is-dark">Backend</span>
+    </a>
+    <a class="nes-badge" style="cursor: default">
+      <span class="is-dark">Backend</span>
+    </a>
+  </div>
+  <p
+    style="
+      font-size: clamp(1rem, 1.2vw, 1.7rem);
+      text-align: justify;
+      margin-bottom: 0.5rem;
+      max-width: 75ch;
+    "
+  >
+    Team task management system. Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Atque autem delectus ut
+    nihil voluptatem, ratione qui illum, quibusdam natus
+    voluptas porro id maxime doloribus ullam, voluptate nemo
+    placeat magni quod!
+  </p>
+  <a
+    href=""
+    style="
+      cursor: pointer;
+      color: inherit;
+      text-decoration: underline #212529 solid 2px;
+      margin-bottom: 4px;
+    "
+  >
+    GitHub
+  </a>
+  <a
+    href=""
+    style="
+      cursor: pointer;
+      color: inherit;
+      text-decoration: underline #212529 solid 2px;
+      margin-bottom: 4px;
+    "
+  >
+    Web App
+  </a>
 </div> */
 }
